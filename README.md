44208ec5c1518d429cc5ef1fc5edc37473451fa3
Serge Dukic <serge.dukic@clearpoint.co.nz>
2016-12-20 15:08:26 +1300

    Filled out README read in file
    Basic loop to read in CSV file and prompt user to specify category. Put a bunch of TODO's in there as well.
    
    Issue(s): None

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Transaction Classifier
 
+## About
 Program to take my bank account statements and classify them. The idea being that I should be able to break down where
 my money has been going.
+
+## Background
+I like knowing where my money is going. It seems like every fucking month it just disappears into thin air.
+
+So, previously, I would keep a budget "spreadsheet" and go through bank statements and assign each transaction (usually
+something I spent money on) to a "category". This worked well... until I had kids and had no time to spend an hour a
+week doing a budget.
+
+This is where this comes in. Basically, it should (over time) start slowly automating the manual classification of this
+crap.
--- transact_classifier.py
+++ transact_classifier.py
@@ -1,6 +1,6 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 
-import sys
+import sys, os
 
 print("Welcome to the Transaction Classifier!")
 
@@ -11,4 +11,48 @@ if len(sys.argv) < 2:
 
 fileToLoad = sys.argv[1]
 
-print('CSV file to load: ' + fileToLoad)
+# Load file
+#print('CSV file to load: ' + fileToLoad)
+try:
+    transactionFile = open(fileToLoad)
+except FileNotFoundError:
+    print('Error loading file: File not found \'' + fileToLoad + '\'')
+except:
+    print('Error loading file: ', sys.exc_info())
+
+# Read in file contents
+fileLines = transactionFile.readlines()
+
+# TODO: We might want to add a "format guesser" if we wanted to support other
+# formats/banks in the future
+
+# TODO: Need to skip the top metadata section and headers
+# TODO: We might want to keep the metadata section for future reference
+headersSection = True
+for line in fileLines:
+    #print('--- ' + line)
+
+    if line == '\n' and headersSection:
+        print('end of headers')
+        headersSection = False
+        continue
+
+    if not headersSection:
+        tokens = line.split(',')
+        print(str(tokens))
+
+        print('Date: ' + tokens[0])
+        print('Unique Id: ' + tokens[1])
+        print('Tran Type: ' + tokens[2])
+        print('Cheque Number: ' + tokens[3])
+        print('Payee: ' + tokens[4])
+        print('Memo: ' + tokens[5])
+        print('Amount: ' + tokens[6])
+
+        # TODO: Prompt user to classify transaction
+        print('What type of transaction is this?')
+        category = input()
+        print('User said it was ' + category + ' category')
+
+transactionFile.close()
+
