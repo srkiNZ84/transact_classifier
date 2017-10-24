ec369643c4bd4c0aac8360fe52e8c5c4616f387e
Serge Dukic <serge.dukic@clearpoint.co.nz>
2017-10-24 22:16:34 +1300

    Switch to logging instead of print
    In the main transaction classifier, switch to the logging library. Also, take out the ElastcSearch stuff at the moment,
    keep it simple.
    
    Got everything into a working order again.
    
    Issue(s): None

--- transact_classifier.py
+++ transact_classifier.py
@@ -1,23 +1,26 @@
 #!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 
 import sys, os, pprint, shelve, csv, requests, json
+import logging
 from datetime import datetime
 from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text
 from elasticsearch_dsl.connections import connections
 
 print("Welcome to the Transaction Classifier!")
 
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
 databaseName = 'transactionData'
 esHostname = 'http://192.168.99.100:9200'
 esIndexName = 'transactions'
 
 # Connect to ElasticSearch and make sure indexes are there
-createIndex = requests.put(esHostname + '/' + esIndexName )
-createIndex.raise_for_status()
+#createIndex = requests.put(esHostname + '/' + esIndexName )
+#createIndex.raise_for_status()
 
 if len(sys.argv) < 2:
-    print('Error: Need to specify file to load')
-    print('Usage: transaction_classifier.py [CSV file to load]')
+    logging.error('Error: Need to specify file to load')
+    logging.error('Usage: transaction_classifier.py [CSV file to load]')
     sys.exit()
 
 fileToLoad = sys.argv[1]
@@ -25,14 +28,16 @@ fileToLoad = sys.argv[1]
 # Load file
 #print('CSV file to load: ' + fileToLoad)
 try:
+    logging.info('Loading file ' + fileToLoad)
     transactionFile = open(fileToLoad)
 except FileNotFoundError:
-    print('Error loading file: File not found \'' + fileToLoad + '\'')
+    logging.error('Error loading file: File not found \'' + fileToLoad + '\'')
 except:
-    print('Error loading file: ', sys.exc_info())
+    logging.error('Error loading file: ', sys.exc_info())
 
 # Read in file contents
-fileLines = list(transactionReader)
+logging.info('Reading file contents')
+fileLines = list(transactionFile)
 
 # TODO: We might want to add a "format guesser" if we wanted to support other
 # formats/banks in the future
@@ -48,49 +53,53 @@ print('To finish classifying categories type \'end\'.')
 # classifications
 try:
     #TODO: Put the categories in ElasticSearch as well
+    logging.info('using ' + databaseName + ' to store classified data')
     transactionFile = shelve.open(databaseName)
     if 'categories' in transactionFile:
-        print('database categories are: ' + str(transactionFile['categories']))
+        logging.debug('Loaded database categories are: ' + str(transactionFile['categories']))
         categories = transactionFile['categories']
 
     if 'classifications' in transactionFile:
-        print('database classifications are: ' + str(transactionFile['classifications']))
+        logging.debug('Loaded database classifications are: ' + str(transactionFile['classifications']))
         classifications = transactionFile['classifications']
 
 except:
-    print('Error opening database file: ', sys.exc_info())
+    logging.error('Error opening database file: ', sys.exc_info())
 
 
 for line in fileLines:
-    #print('--- ' + line)
+    logging.debug('--- Current line is ' + line)
 
-    if line == [] and headersSection:
-        print('end of headers')
+    if line == "\n" and headersSection:
+        logging.debug('Reached the end of headers')
         headersSection = False
         continue
 
     if not headersSection:
-        # TODO: Really should make this a dictionary or a proper object type
-        print('Date: ' + line[0])
-        print('Unique Id: ' + line[1])
-        print('Tran Type: ' + line[2])
-        print('Cheque Number: ' + line[3])
-        print('Payee: ' + line[4])
-        print('Memo: ' + line[5])
-        print('Amount: ' + line[6])
+        sections = line.split(',')
 
-        transactionRecord = {"Date": line[0], "Unique Id": line[1]}
+        # TODO: Really should make this a dictionary or a proper object type
+
+        transactionRecord = {"Date": sections[0], "Unique Id": sections[1]}
 
         # Skip if transaction is already in DB
-        if line[1] in classifications:
-            print('Skipping transaction as it is already been classified as ' + classifications[line[1]])
+        if sections[1] in classifications:
+            logging.info('Skipping transaction as it is already been classified as ' + classifications[sections[1]])
             continue
 
         # TODO: Skip putting into ElasticSearch if it's already in there
 
         # Put the data into elasticsearch
-        putDataRequest = requests.put('http://' + esHostname + '/' + \
-        esIndexName + '/transactionRecord', data = json.dumps(transactionRecord))
+        #putDataRequest = requests.put('http://' + esHostname + '/' + \
+        #esIndexName + '/transactionRecord', data = json.dumps(transactionRecord))
+
+        print('Date: ' + sections[0])
+        print('Unique Id: ' + sections[1])
+        print('Tran Type: ' + sections[2])
+        print('Cheque Number: ' + sections[3])
+        print('Payee: ' + sections[4])
+        print('Memo: ' + sections[5])
+        print('Amount: ' + sections[6])
 
         # Prompt user to classify transaction
         print('#-----------#\nWhat type of transaction is this?')
@@ -98,30 +107,30 @@ for line in fileLines:
         if category == 'end':
             break
 
-        # TODO: Check that the user hasn't entered a blank line
+        # TODO: Check that the user hasn't entered a blank sections
 
         # Save the Unique ID and category
-        classifications[line[1]] = category
+        classifications[sections[1]] = category
 
         # Store the category (if new) and increment total
         if category not in categories.keys():
-            categories[category] = float(line[6])
+            categories[category] = float(sections[6])
         else:
-            categories[category] += float(line[6])
+            categories[category] += float(sections[6])
 
-        print("Categories: " + str(categories))
+        #print("Categories: " + str(categories))
 
 # Save the data to an actual file
-print('Saving to file...')
+logging.info('Saving to database file...')
 transactionFile['categories'] = categories
 transactionFile['classifications'] = classifications
 transactionFile.close()
 
-print('All done!')
-print('categorized transactions:')
-pprint.pprint(classifications)
+logging.info('All done!')
+logging.debug('categorized transactions:')
+logging.debug(pprint.pprint(classifications))
 
-print('Totals are:')
-pprint.pprint(categories)
+logging.debug('Totals are:')
+logging.debug(pprint.pprint(categories))
 for category in categories:
-    print('' + category + ', ' + str(categories[category]))
+    logging.debug('' + category + ', ' + str(categories[category]))
